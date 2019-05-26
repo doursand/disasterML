76b757bfdfb7102cf30f4de151f2ced97064d7e8
doursand <37497557+doursand@users.noreply.github.com>
2019-05-26 19:58:28 +0200

    new version

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -21,20 +21,32 @@ from sklearn.model_selection import GridSearchCV
 
 
 def load_data(database_filepath):
+     '''
+    Load data from database
+    Input:
+        database_filepath: str. Sqlite database path
+    Output:
+        X: Dataframe. features
+        Y: Dataframe. independant variables
+        categories_list: List. list of all categories   
+    '''
+    
     engine = create_engine('sqlite:///disasterdb.db')
-
+    
     df = pd.read_sql_table('disaster',con=engine)
+    categories_list = list(df.columns[:4])
     X = df['message']
-    Y = df[['related-1', 'request-0', 'offer-0', 'aid_related-0', 'medical_help-0', 'medical_products-0', 
-            'search_and_rescue-0', 'security-0', 'military-0', 'child_alone-0', 'water-0', 'food-0', 
-            'shelter-0', 'clothing-0', 'money-0', 'missing_people-0', 'refugees-0', 'death-0', 'other_aid-0', 
-            'infrastructure_related-0', 'transport-0', 'buildings-0', 'electricity-0', 'tools-0', 'hospitals-0', 
-            'shops-0', 'aid_centers-0', 'other_infrastructure-0', 'weather_related-0', 'floods-0', 'storm-0', 
-            'fire-0', 'earthquake-0', 'cold-0', 'other_weather-0', 'direct_report-0']]
-
+    Y = df[categories_list]
+    return X, Y, categories_list
 
 def tokenize(text):
-        # Normalize Text, lower case
+    '''
+    Process and tokenize text
+    Input:
+        text: str. Sqlite database path
+    
+    '''
+    # Normalize Text, lower case
     text = re.sub(r"[^a-zA-Z0-9]", ' ', text.lower())
     # Tokenize
     tokens = word_tokenize(text)
@@ -53,6 +65,11 @@ def tokenize(text):
 
 
 def build_model():
+    '''
+    Build the pipeline for the ML model. Model is optimized thru gridsearch
+    
+    
+    '''
     pipeline = Pipeline([
     ('vect', CountVectorizer(tokenizer=tokenize)),
     ('tfidf', TfidfTransformer()),
@@ -77,6 +94,12 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
+    '''
+    Save the trained model
+    Input:
+        model: pipeline model
+        model_filepath: str. Path of the saved model
+    '''
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
